becee421aaf66ba6f3ccfe2182f9d365a8b2c4c9
Muhammad Hassan <hassan0501651@gmail.com>
2026-09-04 22:22:00 +0500

    remove dist folder

--- dist/src/utils/AppError.js
+++
@@ -1,8 +0,0 @@
-export default class AppError extends Error {
-    statusCode;
-    constructor(message, statusCode) {
-        super(message);
-        this.name = "AppError";
-        this.statusCode = statusCode;
-    }
-}
