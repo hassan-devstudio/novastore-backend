f94bf58ac952bf5c06bd8ae4579eb9f335a94ccc
Muhammad Hassan <hassan0501651@gmail.com>
2026-09-10 09:13:44 +0500

    feat(product): add error handling for missing product ID in update and delete routes

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -7,13 +7,21 @@ import {
   deleteProduct,
 } from "../controllers/productController.js";
 import validateToken from "../middleware/validateTokenHandler.js";
+import AppError from "../utils/AppError.js";
+import constants from "../constants/constants.js";
 
 const router = express.Router();
 
 router.post("/", validateToken, createProduct);
 router.get("/", validateToken, getProducts);
 router.get("/:id", validateToken, getProductById);
+router.patch("/", (_req, _res, next) =>
+  next(new AppError("Product ID is required", constants.BAD_REQUEST)),
+);
 router.patch("/:id", validateToken, updateProduct);
+router.delete("/", (_req, _res, next) =>
+  next(new AppError("Product ID is required", constants.BAD_REQUEST)),
+);
 router.delete("/:id", validateToken, deleteProduct);
 
 export default router;
